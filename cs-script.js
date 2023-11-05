7509a7ccb86145ce9961a12b267328fdc78a705b
aly_dev07 <aly_dev07@yahoo.com>
2023-11-05 14:03:40 +1300

    refreshing mini cart and enabled cart slider for custom product button

--- cs-script.js
+++ cs-script.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function(){
 
     jQuery(".sc_ajax_add_to_cart").click(function(e) {
         e.preventDefault();
+        jQuery(this).addClass("loading");
 
         var n_data = {
 			action: 'cs_add_to_cart',
@@ -14,7 +15,23 @@ jQuery(document).ready(function(){
             if(response.indexOf("ERROR") != -1){
 				alert("Error occured when setting up the delivery. " + response);
 			} else {
-                location.reload();
+
+                var url = woocommerce_params.wc_ajax_url;
+                url = url.replace("%%endpoint%%", "get_refreshed_fragments");
+                jQuery.post(url, function (data, status) {
+
+                    jQuery(".woocommerce.widget_shopping_cart").html(data.fragments["div.widget_shopping_cart_content"]);
+                    if (data.fragments) {
+                        jQuery.each(data.fragments, function (key, value) {
+
+                        jQuery(key).replaceWith(value);
+                        });
+                    }
+                    jQuery("body").trigger("wc_fragments_refreshed");
+                    jQuery(".sc_ajax_add_to_cart").removeClass("loading");
+
+                    jQuery("#fkcart-modal").addClass("fkcart-show").show();
+                });
 			}
 
 		});
